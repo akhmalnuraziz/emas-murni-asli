b75266d6f1785ea49d19883dad8ddc47e080003d
Akhmal <akhmalnuraziz01@gmail.com>
2026-05-26 11:56:20 +0700

    fix: sidebar visible on desktop

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -60,7 +60,8 @@ export default function Sidebar({ mobileOpen, onClose }: SidebarProps) {
         'transition-transform duration-300 ease-in-out',
         // Desktop: selalu tampil (translate-x-0)
         // Mobile: tampil hanya jika mobileOpen
-        mobileOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0',
+        '-translate-x-full lg:translate-x-0',
+mobileOpen && 'translate-x-0',
       )}>
 
         {/* Logo / Brand */}
